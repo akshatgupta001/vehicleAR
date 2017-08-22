999038f7cfa6778c5fd68fdb11700605345dc93b
Rayan Slim <rayanslim@Rayans-MBP.lan>
2017-08-22 15:56:39 +0300

    vehicle 1

--- Vehicle/ViewController.swift
+++ Vehicle/ViewController.swift
@@ -26,19 +26,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func createLava(planeAnchor: ARPlaneAnchor) -> SCNNode {
-        let lavaNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(CGFloat(planeAnchor.extent.z))))
-        lavaNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "lava")
-        lavaNode.geometry?.firstMaterial?.isDoubleSided = true
-        lavaNode.position = SCNVector3(planeAnchor.center.x,planeAnchor.center.y,planeAnchor.center.z)
-        lavaNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
-        return lavaNode
+    func createConcrete(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let concreteNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(CGFloat(planeAnchor.extent.z))))
+        concreteNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "concrete")
+        concreteNode.geometry?.firstMaterial?.isDoubleSided = true
+        concreteNode.position = SCNVector3(planeAnchor.center.x,planeAnchor.center.y,planeAnchor.center.z)
+        concreteNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
+        let staticBody = SCNPhysicsBody.static()
+        concreteNode.physicsBody = staticBody
+        return concreteNode
     }
 
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
-        let lavaNode = createLava(planeAnchor: planeAnchor)
-        node.addChildNode(lavaNode)
+        let concreteNode = createConcrete(planeAnchor: planeAnchor)
+        node.addChildNode(concreteNode)
         print("new flat surface detected, new ARPlaneAnchor added")
         
     }
@@ -50,8 +52,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             childNode.removeFromParentNode()
             
         }
-        let lavaNode = createLava(planeAnchor: planeAnchor)
-        node.addChildNode(lavaNode)
+        let concreteNode = createConcrete(planeAnchor: planeAnchor)
+        node.addChildNode(concreteNode)
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
@@ -62,8 +64,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
 
     }
+    
+    @IBAction func addCar(_ sender: Any) {
+        guard let pointOfView = sceneView.pointOfView else {return}
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
+        let location = SCNVector3(transform.m41,transform.m42,transform.m43)
+        let currentPositionOfCamera = orientation + location
+        let box = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        box.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        box.position = currentPositionOfCamera
+        let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: box, options: [SCNPhysicsShape.Option.keepAsCompound: true]))
+        box.physicsBody = body
+
+        self.sceneView.scene.rootNode.addChildNode(box)
+    }
+    
 }
 
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+    
+}
 extension Int {
     
     var degreesToRadians: Double { return Double(self) * .pi/180}
